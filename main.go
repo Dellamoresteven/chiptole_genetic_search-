adc70e1ec64bd940d2989c9d6536ee104999843c
Dellamoresteven <dellamoresteven@gmail.com>
2020-08-10 14:10:24 -0500

    Working

--- main.go
+++ main.go
@@ -8,14 +8,15 @@ package main
 /*
 Notes:
 Init Travelers
-✓	- Varible length dna
+✓	- Variable length dna
 ✓	- Randomly selected from the Stores list
+✓	- Make sure they all start at the same store
 Playing the game
-✓	- Need to use go routies to speed this up.
+✓	- Need to use go routine to speed this up.
 ✓	- Count the number of unique stores the Travel has visited
 ✓	- Track the distance the traveler has traveled
-	- Create a score algorthm to rate how well a travel did
-		- Need to visit every unique store at least 1
+	- Create a score algorithm to rate how well a travel did
+✓		- Need to visit every unique store at least 1
 		- Travel the least amount of distance
 */
 
@@ -59,13 +60,14 @@ func newTraveler(dna []Store) Traveler {
 }
 
 const (
-	numGames     = 2   // Number of games to be played
+	numGames     = 100 // Number of games to be played
 	population   = 100 // Number of Travelers
 	mutationRate = .05 // Mutation rate of the dna of travelers
 )
 
-var travlers []Traveler // List of Travelers that will take on the chiptole world
-var stores []Store      // The metadata of the stores read from the csv
+var travelers []Traveler // List of Travelers that will take on the chiptole world
+var stores []Store       // The metadata of the stores read from the csv
+var gameNumber int = 0
 
 func main() {
 	csvfile, err := os.Open("data/datasets_804019_1378604_chipotle_stores.csv")
@@ -103,14 +105,17 @@ func startGame() {
 	initPopulation()
 	var wg sync.WaitGroup
 	for i := 0; i < numGames; i++ {
-		fmt.Println("\n\nNEW GAME STARTED\n\n")
+		gameNumber++
+		// fmt.Println("\n\nNEW GAME STARTED\n\n")
 		for j := 0; j < population; j++ {
 			wg.Add(1)
-			go playGame(&travlers[j], &wg)
+			go playGame(&travelers[j], &wg)
 		}
 		wg.Wait()
 		breedNextGeneration()
 	}
+
+	printBestTraveler()
 }
 
 func initPopulation() {
@@ -118,11 +123,12 @@ func initPopulation() {
 	r1 := rand.New(s1)
 	for i := 0; i < population; i++ {
 		var dna []Store
-		for j := 0; j < r1.Intn(len(stores)-1000)+1000; j++ {
+		dna = append(dna, stores[0])
+		for j := 1; j < r1.Intn(len(stores)-1000)+1000; j++ {
 			dna = append(dna, stores[r1.Intn(len(stores))])
 		}
 		t := newTraveler(dna)
-		travlers = append(travlers, t)
+		travelers = append(travelers, t)
 	}
 }
 
@@ -137,37 +143,65 @@ func playGame(t *Traveler, wg *sync.WaitGroup) {
 		t.numStores[newStore] = true
 		oldStore = newStore
 	}
-	t.score = float64(len(t.numStores))
-	t.distance = 0
+	t.score = float64(len(t.numStores)) + (1 / t.distance)
 }
 
 func breedNextGeneration() {
-	sort.SliceStable(travlers, func(i, j int) bool {
-		return travlers[i].score > travlers[j].score
+	sort.SliceStable(travelers, func(i, j int) bool {
+		return travelers[i].score > travelers[j].score
 	})
 
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
-	travlers = travlers[:len(travlers)-3] // remove the worst 3 performing travlers
-	travlers = append(travlers, breed(travlers[r1.Intn(10)+1], travlers[r1.Intn(10)+1]))
-	travlers = append(travlers, breed(travlers[r1.Intn(10)+1], travlers[r1.Intn(10)+1]))
-	travlers = append(travlers, breed(travlers[r1.Intn(10)+1], travlers[r1.Intn(10)+1]))
+	travelers = travelers[:len(travelers)-3] // remove the worst 3 performing travelers
+	travelers = append(travelers, breed(travelers[r1.Intn(10)+1], travelers[r1.Intn(10)+1]))
+	travelers = append(travelers, breed(travelers[r1.Intn(10)+1], travelers[r1.Intn(10)+1]))
+	travelers = append(travelers, breed(travelers[r1.Intn(10)+1], travelers[r1.Intn(10)+1]))
 
-	for _, t := range travlers {
-		fmt.Println(t.score)
+	maxScore := 0.0
+	for _, t := range travelers {
+		// fmt.Println(t.score)
+		if maxScore < t.score {
+			maxScore = t.score
+		}
 		t.score = 0
+		t.numStores = make(map[Store]bool)
+		t.distance = 0
 	}
+	fmt.Println("Max Score for game ", gameNumber, " : ", maxScore)
 }
 
 func breed(p1, p2 Traveler) (child Traveler) {
 	var dna []Store
 
-	min := len(p1.dna)
-	if min > len(p2.dna) {
-		min = len(p2.dna)
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	if mutationRate > float64((r1.Intn(100) / 100)) {
+		dna = append(dna, stores[0])
+		for j := 1; j < r1.Intn(len(stores)-1000)+1000; j++ {
+			dna = append(dna, stores[r1.Intn(len(stores))])
+		}
+		child = newTraveler(dna)
 	}
-	crossover := min / 2
+
+	dna = append(dna, p1.dna[0:len(p1.dna)/2]...)
+	dna = append(dna, p2.dna[len(p2.dna)/2:len(p2.dna)]...)
+	// fmt.Println("p1: ", p1.dna[1:2])
+	// fmt.Println("p2: ", p2.dna[len(p2.dna)-1:])
+	// fmt.Println("CHILD START: ", dna[1:2])
+	// fmt.Println("CHILD END: ", dna[len(dna)-1:], "\n\n\n")
 
 	child = newTraveler(dna)
 	return
+}
+
+func printBestTraveler() {
+	sort.SliceStable(travelers, func(i, j int) bool {
+		return travelers[i].score > travelers[j].score
+	})
+	t := travelers[0]
+	fmt.Println("\n\nBEST")
+	fmt.Println("Score: ", t.score)
+	fmt.Println("numStores: ", len(t.numStores))
+	fmt.Println("distance: ", t.distance)
 }
